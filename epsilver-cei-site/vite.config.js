9dacadd411da2aee3920d5aefb23e17fd00d9518
epsilver <alexis@epsilver.xyz>
2026-03-07 22:11:39 -0500

    Fix base path for custom domain.

--- epsilver-cei-site/vite.config.js
+++ epsilver-cei-site/vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 import path from "node:path";
 
 export default defineConfig({
-  base: "/epsilver-cei/",
+  base: "/",
   plugins: [
     {
       name: "profiles-reload",
