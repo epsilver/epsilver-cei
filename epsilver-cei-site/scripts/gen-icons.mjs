2642bccc15b3d42673ce788749504cfee1f7c8db
epsilver <alexis@epsilver.xyz>
2026-03-08 00:10:48 -0500

    Add Twitter avatar

--- epsilver-cei-site/scripts/gen-icons.mjs
+++ epsilver-cei-site/scripts/gen-icons.mjs
@@ -1,12 +1,14 @@
 import sharp from "sharp";
 import { readFileSync } from "node:fs";
 
-const favicon = readFileSync("public/favicon.svg");
-const ogSvg   = readFileSync("public/og-image.svg");
+const favicon  = readFileSync("public/favicon.svg");
+const ogSvg    = readFileSync("public/og-image.svg");
+const twitterSvg = readFileSync("public/twitter-avatar.svg");
 
 await sharp(favicon).resize(16,  16).toFile("public/favicon-16x16.png");
 await sharp(favicon).resize(32,  32).toFile("public/favicon-32x32.png");
 await sharp(favicon).resize(180,180).png().toFile("public/apple-touch-icon.png");
 await sharp(ogSvg).resize(1200, 630).png().toFile("public/og-image.png");
+await sharp(twitterSvg).resize(400, 400).png().toFile("public/twitter-avatar.png");
 
 console.log("Icons generated.");
