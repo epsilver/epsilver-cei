8eba3a6342f3e3cfd1fd2bced9d66230736b96dc
epsilver <alexis@epsilver.xyz>
2026-03-10 12:12:15 -0400

    Reduce profile name font size to 12px on mobile

--- epsilver-cei-site/src/pages/home.js
+++ epsilver-cei-site/src/pages/home.js
@@ -83,7 +83,7 @@ export async function HomePage(root) {
 
       item.innerHTML = `
         <div class="left">
-          <div style="font-size:14px"><b>${p.name}</b></div>
+          <div class="profile-name"><b>${p.name}</b></div>
           <span class="lean ${leanClass}">${lean}</span>
         </div>
         <div class="pill">${pill}</div>
